36701508149c558c576512dc399c7c5ef3542650
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-12 23:11:30 +0530

    added mongodb error logging

--- server.js
+++ server.js
@@ -65,11 +65,12 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`🚀 Server running`);
     });
   })
   .catch((err) => {
-    console.error('❌ MongoDB connection failed:', err.message);
+    console.error('❌ FULL MONGODB ERROR:');
+    console.error(err);
     process.exit(1);
   });

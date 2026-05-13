d62e32bcc48b244725c829884dfe48019031bfca
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-05-13 11:08:33 +0530

    update

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ['farmer', 'consumer', 'admin'],
+      enum: ['farmer', 'consumer', 'admin','delivery'],
       default: 'consumer'
     },
 
